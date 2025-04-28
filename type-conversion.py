c015cb7de1295047fba437e7d329df7cee6d8d47
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-04-28 20:44:10 +0530

    Type conversion

---
+++ type-conversion.py
@@ -0,0 +1,17 @@
+# Type conversion in Python
+a = 5
+b = 2.5
+
+sum = a + b
+
+print(sum) # 7.5
+print(type(sum)) # <class 'float'>
+
+# Type casting
+a = 5
+b = 2.5
+
+sum = a + int(b)
+
+print(sum) # 7
+print(type(sum)) # <class 'int'>
