05556b3e72d069bd9ccc00979347323c7ffda4f4
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-04-28 20:54:47 +0530

    Print the avg of two floating point numbers

---
+++ floating-point-number-print-theere-avg.py
@@ -0,0 +1,6 @@
+a = float(input("Enter a floating-point number: "))
+b = float(input("Enter another floating-point number: "))
+
+# Calculate the average
+avg = (a + b) / 2
+print("The average of", a, "and", b, "is:", avg) # Prints the average of a and b
