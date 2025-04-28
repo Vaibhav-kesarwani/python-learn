c77acb9e0361bd8fe257c594a3f2eb19d473ac1b
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-04-28 20:50:53 +0530

    input

---
+++ input.py
@@ -0,0 +1,8 @@
+# To take the input from the user
+
+name = input("Enter Your name: ")
+print("Hello " + name) # Takes String as input always
+
+a = int(input("Enter a number: ")) # Takes Integer as input
+b = float(input("Enter a float number: ")) # Takes Float as input
+print("The sum of a and b is: ", a + int(b)) # Prints the sum of a and b
