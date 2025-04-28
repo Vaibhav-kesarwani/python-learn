0aa1f9c51cb9f0aad7707e2c1eb3d60a6cae10a7
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-04-28 20:53:35 +0530

    Print the area of the square

---
+++ print-the-area-of-the-square.py
@@ -0,0 +1,4 @@
+sides = int(input("Enter the length of the sides of the square: ")) # Takes Integer as input
+area = sides * sides # Calculates the area of the square
+
+print("The area of the square is: ", area) # Prints the area of the square
