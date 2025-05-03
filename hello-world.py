005bda0cf1d5c83bcf3e14a8d09c00e583b44d55
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-03 09:06:48 +0530

    remove the beginner files

--- hello-world.py
+++
@@ -1,7 +0,0 @@
-print("Hello World");
-print("Hello i am vaibhav kesarwani a 3rd year student of computer science");
-print("I am learning python programming language");
-
-print(10)
-print(2 + 3)
-print(5 > 4)
