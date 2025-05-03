ec8936b3c60c09dae325f9dae6c0bc99ad1f81ef
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-03 09:07:17 +0530

    Remove the stashed files

--- print-the-area-of-the-square.py
+++
@@ -1,4 +0,0 @@
-sides = int(input("Enter the length of the sides of the square: ")) # Takes Integer as input
-area = sides * sides # Calculates the area of the square
-
-print("The area of the square is: ", area) # Prints the area of the square
